a276e0e2c470c549fa0fa9bfcb9eb7d544c87d9d
胡太极 <tother@TOTHERMacBook-Pro.local>
2026-08-28 02:09:50 +0800

    主题扩散动画时长调整:2400ms -> 4000ms(看效果)

--- theme.js
+++ theme.js
@@ -54,8 +54,8 @@ function lmgAnimateToggle(btn, toDark) {
                 ]
             },
             {
-                // 2400ms 慢速蔓延
-                duration: 2400,
+                // 4000ms 慢速蔓延
+                duration: 4000,
                 easing: 'cubic-bezier(0.16, 0.8, 0.3, 1)',
                 pseudoElement: '::view-transition-new(root)'
             }
